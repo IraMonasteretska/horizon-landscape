deb35475938cb637e42b6e89ce1cff7f06e2c883
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2024-07-17 19:10:51 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+$(function () {
+
+    // main slider
+
+    var swiper = new Swiper(".heroscreen__slider", {
+        effect: "fade",
+        loop: true,
+        speed: 1000,
+        autoplay: {
+          delay: 1600,
+        },
+        keyboard: {
+            enabled: true,
+        },
+        direction: 'vertical',
+
+        scrollbar: {
+            el: ".swiper-scrollbar",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            type: "fraction",
+            type: 'custom',
+            renderCustom: function (swiper, current, total) {
+                return `<span class="swiper-pagination-current"> 0${current} </span>  <span class="swiper-pagination-current">0${total}</span>`;
+            }
+        },
+    });
+});
